b8357b6b2981a6e6bb80a0005de50e16216f584d
FranklinZ12 <franklinq8@gmail.com>
2023-05-09 19:06:32 -0500

    agegando ruta directa

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post('/api/auth/new',
     crearUsuario
 );
 
-router.post('/api/auth/',
+router.post('https://fundacion-transformando-con-pazion-api.vercel.app/api/auth/',
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe de ser de 6 caracteres o mas').isLength({ min: 6 }),
