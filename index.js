dbb082ba9ebe37c9f6655d23c96ac8852291c5b3
FranklinZ12 <franklinq8@gmail.com>
2023-05-11 11:53:21 -0500

    agregando allowCors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from "dotenv";
 import router from './routes/auth.js';
 import dbConnection from './database/config.js';
-import cors from "cors";
+import allowCors from './allowCors.js';
+// import cors from "cors";
 
 
 //VARIABLES DE ENTORNO
@@ -15,28 +16,22 @@ const app = express();
 dbConnection();
 
 //ACTIVACIÓN DE CORS
-app.use(cors({
-    origin: 'https://fundacion-transformando-con-pazion.vercel.app',
-    "options":{
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    }
-}
-));
+// app.use(cors({
+//     origin: 'https://fundacion-transformando-con-pazion.vercel.app',
+//     "options":{
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//         "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     }
+// }
+// ));
+app.use(allowCors);
 
 // Directorio Público
 app.use(express.static('public'));
 
 //Lectura y parseo del body
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://fundacion-transformando-con-pazion.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
 
 app.use(router);
 
