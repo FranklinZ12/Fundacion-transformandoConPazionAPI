5161d69b9bdb140fe73d938a2feb670f1480681a
FranklinZ12 <franklinq8@gmail.com>
2023-05-12 18:28:59 -0500

    intento mil de cors

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ dotenv.config();
 //ACTIVACIÓN DE EXPRESS
 const app = express();
 
+//ACTIVACIÓN DE CORS
+app.use(cors());
+
 //Base de datos
 dbConnection();
 
-//ACTIVACIÓN DE CORS
 // app.use(cors({
 //     origin: ['https://fundacion-transformando-con-pazion.vercel.app','https://fundacion-transformando-con-pazion-ieyy85za5-franklinz12.vercel.app'],
 //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -36,6 +38,13 @@ dbConnection();
 //Lectura y parseo del body
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.options('*', cors());
 app.use(router);
 
