50fbb847782fff69efaf2a68fe11d91603de660a
FranklinZ12 <franklinq8@gmail.com>
2023-05-11 20:13:17 -0500

    cambiando cors

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ dbConnection();
 //ACTIVACIÓN DE CORS
 app.use(cors({
     origin: '*',
-    "options":{
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    }
 }
 ));
 // app.use(allowCors);
